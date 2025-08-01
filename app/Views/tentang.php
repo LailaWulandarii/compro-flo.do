c0139edcdff7e50dee8c6adf4611ab9e3450e842
LailaWulandarii <E41220418@student.polije.ac.id>
2025-08-01 20:21:18 +0700

    ass logic on tentang view and about controller

--- app/Views/tentang.php
+++ app/Views/tentang.php
@@ -7,28 +7,24 @@
     <div class="container">
       <div class="tentang-content">
         <h1 class="h1 section-title">
-          <span class="has-before" style="color: black;">Tentang</span>
+          <span class="has-before" style="color: black;">
+            <?= $lang == 'id' ? $meta['nama_halaman_id'] : $meta['nama_halaman_en']; ?>
+          </span>
         </h1>
-        <h2 class="text-c">Flo.do</h2>
+        <h2 class="text-c"><?= $profil['nama_perusahaan'] ?? 'Flo.do'; ?></h2>
       </div>
     </div>
     <div class="container">
       <figure class="tentang-banner">
-        <img src="./assets/img/tentang-1.jpg" style="border-radius: 30px;" width="355" height="356"
-          loading="lazy" alt="tentang banner" class="w-100">
+        <img src="<?= base_url('assets/img/profil/' . $profil['foto_perusahaan']); ?>"
+          alt="<?= $lang == 'id' ? $profil['alt_foto_perusahaan_id'] : $profil['alt_foto_perusahaan_en']; ?>"
+          style="border-radius: 30px;" width="355" height="356" loading="lazy" class="w-100">
       </figure>
 
       <div class="tentang-content">
-
-        <p class="card-text">Flo.do adalah florist dan penyedia dekorasi interior yang menghadirkan keindahan alami
-          ke dalam ruang Anda. Kami menggabungkan estetika bunga dengan desain ruang yang harmonis, menciptakan
-          atmosfer yang elegan dan penuh makna untuk berbagai kebutuhan—mulai dari pernikahan, acara spesial, hingga
-          interior hunian dan kantor.</p><br>
         <p class="card-text">
-          Dengan sentuhan artistik dan pengalaman profesional, Flo.do telah dipercaya oleh ratusan klien. Kami
-          merancang setiap proyek dengan perhatian terhadap detail, mengutamakan keunikan dan kenyamanan visual.
-          Melalui kolaborasi kreatif dan layanan personal, kami berkomitmen menjadikan setiap ruang lebih hidup dan
-          berkesan.</p>
+          <?= $lang == 'id' ? $profil['deskripsi_perusahaan_id'] : $profil['deskripsi_perusahaan_en']; ?>
+        </p>
       </div>
 
     </div>
