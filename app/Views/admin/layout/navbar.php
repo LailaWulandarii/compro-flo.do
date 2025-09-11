013ded438b9a08dbb35669c8669a780b55065a55
LailaWulandarii <E41220418@student.polije.ac.id>
2025-09-11 14:07:47 +0700

    edit assets url for logo on navbar admin

--- app/Views/admin/layout/navbar.php
+++ app/Views/admin/layout/navbar.php
@@ -3,7 +3,7 @@
     <div class="sidepanel-inner d-flex flex-column">
         <a href="#" id="sidepanel-close" class="sidepanel-close d-xl-none">&times;</a>
         <div class="app-branding">
-            <a class="app-logo" href="<?= base_url('admin/dashboard') ?>"><img class="logo-icon me-2" src="<?= base_url('assets/img/profil/logo2.png') ?>" alt="logo"><span class="logo-text">Furnetic</span></a>
+            <a class="app-logo" href="<?= base_url('admin/dashboard') ?>"><img class="logo-icon me-2" src="<?= base_url('assets/img/profil/logo.jpg') ?>" alt="logo"><span class="logo-text">Flo.do</span></a>
         </div><!--//app-branding-->
 
         <nav id="app-nav-main" class="app-nav app-nav-main flex-grow-1">
