b904324340953f94bbb1c710d1322ca9a338bfa0
LailaWulandarii <E41220418@student.polije.ac.id>
2025-10-23 13:47:19 +0700

    edit background color of article and detail article section

--- app/Views/artikel.php
+++ app/Views/artikel.php
@@ -15,7 +15,7 @@
     ? $allArticle[0]['nama_kategori']
     : null;
   ?>
-  <section class="section aktivitas" id="aktivitas" aria-label="aktivitas">
+  <section class="section aktivitas" id="aktivitas" aria-label="aktivitas" style="background-color: white  ;">
     <div class="container">
       <h1 class="h1 section-title text-center" style="margin-bottom: 10px; font-size: 4rem;">
         <span class="has-before">
--- app/Views/detail_artikel.php
+++ app/Views/detail_artikel.php
@@ -4,7 +4,7 @@
   <?php
   $lang = session()->get('lang') ?? 'id';
   ?>
-  <section class="section aktivitas" id="aktivitas" aria-label="aktivitas">
+  <section class="section aktivitas" id="aktivitas" aria-label="aktivitas" style="background-color: white  ;">
     <div class="container">
       <h1 class="h1 section-title text-center" style="margin-bottom: 10px; font-size: 4rem;">
         <span class="has-before"><?= $lang === 'id' ? $artikel['judul_artikel_id'] : $artikel['judul_artikel_en'] ?></span>
