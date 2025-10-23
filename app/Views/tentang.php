0a15355f7b7a905a4610fc6192051f2d49e08369
LailaWulandarii <E41220418@student.polije.ac.id>
2025-10-23 13:48:25 +0700

    edit background color of about section

--- app/Views/tentang.php
+++ app/Views/tentang.php
@@ -2,35 +2,29 @@
 
 <?= $this->section('content'); ?>
 <article>
-  <section class="section tentang has-bg-image" id="tentang" aria-label="tentang">
+  <section class="section tentang" id="tentang" aria-label="tentang" style="background-color: white; margin-top: 50px">
     <div class="container">
-      <div class="tentang-content">
-        <h1 class="h1 section-title text-c">
-          <span class="has-before" style="color: black;">
-            <?= ($lang == 'id' ? $meta['nama_halaman_id'] : $meta['nama_halaman_en']) . ' - ' . ($profil['nama_perusahaan'] ?? 'Flo.do'); ?>
-          </span>
-        </h1>
-      </div>
-    </div>
 
-    <div class="container">
       <figure class="tentang-banner">
-        <img
-          src="<?= base_url('assets/img/profil/' . $profil['foto_perusahaan']); ?>"
-          alt="<?= $lang == 'id' ? $profil['alt_foto_perusahaan_id'] : $profil['alt_foto_perusahaan_en']; ?>"
-          style="border-radius: 30px;"
-          width="355"
-          height="356"
-          loading="lazy"
-          class="w-100">
+        <img src="<?= base_url('assets/img/profil/' . $profil['foto_perusahaan']); ?>" alt="<?= $lang == 'id' ? $profil['alt_foto_perusahaan_id'] : $profil['alt_foto_perusahaan_en']; ?>" style="border-radius: 30px;" width="355" height="356"
+          loading="lazy" alt="tentang banner" class="w-100">
       </figure>
 
       <div class="tentang-content">
+        <h1 class="h1 section-title" style="color: black; margin-bottom: 4px;">
+          <span class="has-before"><?= $lang == 'id' ? $meta['nama_halaman_id'] : $meta['nama_halaman_en']; ?></span>
+        </h1>
+        <p class="section-subtitle" style="margin-bottom: 20px;">
+          <?= $lang == 'id' ? $meta['deskripsi_halaman_id'] : $meta['deskripsi_halaman_en']; ?>
+        </p>
         <p class="card-text">
           <?= $lang == 'id' ? $profil['deskripsi_perusahaan_id'] : $profil['deskripsi_perusahaan_en']; ?>
         </p>
+        <br>
       </div>
+
     </div>
+
   </section>
 </article>
 <?= $this->endSection(); ?>
